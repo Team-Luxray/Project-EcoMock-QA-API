85e34eb31c1e6b9a523655662846014d21016b89
Samantha Pham <samanthavpham@gmail.com>
2022-03-04 15:18:29 -0800

    completed rough draft of queries - need to test post and put requests

--- server/router.js
+++ server/router.js
@@ -43,9 +43,11 @@ router.get('/qa/questions', async (req, res) => {
         FROM answers AS a
         LEFT JOIN photos AS ap
         ON a.answer_id = ap.answer_id
+        WHERE a.answer_reported = false
       ) AS aap
       ON q.question_id = aap.question_id
-      WHERE q.product_id = $1`,
+      WHERE q.product_id = $1
+      AND q.question_reported = false`,
       [product_id]
     );
     res.status(200).send(rows);
@@ -71,7 +73,8 @@ router.get('/qa/questions/:question_id/answers', async (req, res) => {
       FROM answers AS a
       LEFT JOIN photos AS ap
       ON a.answer_id = ap.answer_id
-      WHERE a.question_id = $1`,
+      WHERE a.question_id = $1
+      AND a.answer_reported = false`,
       [question_id]
     );
     res.status(200).send(rows);
@@ -83,12 +86,11 @@ router.get('/qa/questions/:question_id/answers', async (req, res) => {
 // route to add a question for a given product
 router.post('/qa/questions', async (req, res) => {
   try {
-    const { body, name, email, product_id } = req.body;
-    const rows = await db.query(
-      `INSERT INTO
-      questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-      VALUES (null, ?, ?, SELECT now()::timestamp, ?, ?, false, 0)`,
-      [product_id, body, name, email]
+    const { product_id, question_body, asker_name, asker_email } = req.body;
+    const { rows } = await db.query(
+      `INSERT INTO questions
+      VALUES(null, $1, $2, SELECT now()::timestamp, $3, $4, false, 0)`,
+      [product_id, question_body, asker_name, asker_email]
     );
     res.status(201).send(rows);
   } catch (error) {
@@ -99,7 +101,14 @@ router.post('/qa/questions', async (req, res) => {
 // route to add an answer for a given question
 router.post('/qa/questions/:question_id/answers', async (req, res) => {
   try {
-
+    const { question_id } = req.params;
+    const { answer_body, answerer_name, answerer_email } = req.body;
+    const { rows } = await db.query(
+      `INSERT INTO questions
+      VALUES(null, $1, $2, SELECT now()::timestamp, $3, $4, false, 0)`,
+      [question_id, answer_body, answerer_name, answerer_email]
+    );
+    res.status(201).send(rows);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -108,7 +117,14 @@ router.post('/qa/questions/:question_id/answers', async (req, res) => {
 // route to mark a given question as helpful
 router.put('/qa/questions/:question_id/helpful', async (req, res) => {
   try {
-
+    const { question_id } = req.params;
+    const { rows } = await db.query(
+      `UPDATE questions
+      SET question_helpful = question_helpful + 1
+      WHERE question_id = $1`,
+      [question_id]
+    );
+    res.status(200).send(`successfully marked question ${question_id} as helpful`);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -117,7 +133,14 @@ router.put('/qa/questions/:question_id/helpful', async (req, res) => {
 // route to report a given question
 router.put('/qa/questions/:question_id/report', async (req, res) => {
   try {
-
+    const { question_id } = req.params;
+    const { rows } = await db.query(
+      `UPDATE questions
+      SET question_reported = true
+      WHERE question_id = $1`,
+      [question_id]
+    );
+    res.status(200).send(`successfully reported question ${question_id}`);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -126,7 +149,14 @@ router.put('/qa/questions/:question_id/report', async (req, res) => {
 // route to mark a given answer as helpful
 router.put('/qa/answers/:answer_id/helpful', async (req, res) => {
   try {
-
+    const { answer_id } = req.params;
+    const { rows } = await db.query(
+      `UPDATE answers
+      SET answer_helpful = answer_helpful + 1
+      WHERE answer_id = $1`,
+      [answer_id]
+    );
+    res.status(200).send(`successfully marked answer ${answer_id} as helpful`);
   } catch (error) {
     res.status(400).send(error);
   }
@@ -135,7 +165,14 @@ router.put('/qa/answers/:answer_id/helpful', async (req, res) => {
 // route to report a given answer
 router.put('/qa/answers/:answer_id/report', async (req, res) => {
   try {
-
+    const { answer_id } = req.params;
+    const { rows } = await db.query(
+      `UPDATE answers
+      SET answer_reported = true
+      WHERE answer_id = $1`,
+      [answer_id]
+    );
+    res.status(200).send(`successfully reported answer ${answer_id}`);
   } catch (error) {
     res.status(400).send(error);
   }
