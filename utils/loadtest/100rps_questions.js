c1ff0162ef99519c43b5d38018ce8c5b93c38ca3
Samantha Pham <samanthavpham@gmail.com>
2022-03-07 14:56:38 -0800

    added k6 tests for get requests

---
+++ utils/loadtest/100rps_questions.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Counter } from 'k6/metrics';
+
+export const requests = new Counter('http_reqs');
+
+export const options = {
+  vus: 100,
+  duration: '15s'
+};
+
+export default function () {
+  const res = http.get('http://localhost:3000/api/qa/questions');
+  check(res, {
+    'status was 200': (r) => r.status === 200,
+    'transaction time < 50ms': r => r.timings.duration < 50,
+    'transaction time < 100ms': r => r.timings.duration < 100,
+    'transaction time < 200ms': r => r.timings.duration < 200,
+    'transaction time < 500ms': r => r.timings.duration < 500,
+    'transaction time < 1000ms': r => r.timings.duration < 1000,
+    'transaction time < 2000ms': r => r.timings.duration < 2000,
+  });
+  sleep(1);
+}
